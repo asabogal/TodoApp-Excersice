022a536dacb9ee6ce44e24a7583d62d57a8159b6
asabogal <asabogal.git@gmail.com>
2020-10-16 21:26:50 -0400

    move list components to Todo, and pass Todo component

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,9 @@
 import React from 'react';
+// components
+import Todo from './Todo';
 // materialUI
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 
 
@@ -13,9 +13,7 @@ const TodoList = ({todos}) => {
       <List>
       {todos.map(todo => (
         <>
-          <ListItem key={todo.id}>
-            <ListItemText>{todo.task}</ListItemText>
-          </ListItem>
+          <Todo key={todo.id} task={todo.task} completed={todo.completed}/>
           <Divider/>
         </>
       ))}
