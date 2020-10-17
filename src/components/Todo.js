c2ee91b3985172c25e71c3eec8f414da8a191780
asabogal <asabogal.git@gmail.com>
2020-10-17 16:38:06 -0400

    add icons + style text when completed

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,12 +3,25 @@ import React from 'react';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+
 
 const Todo = ({task, completed}) => {
   return (
     <ListItem>
       <Checkbox checked={completed}/>
-      <ListItemText>{task}</ListItemText>
+      <ListItemText style={{textDecoration: completed ? 'line-through' : 'none'}}>{task}</ListItemText>
+      <ListItemSecondaryAction>
+        <IconButton aria-label='Delete'>
+          <DeleteIcon/>
+        </IconButton>
+        <IconButton aria-label='Edit'>
+          <EditIcon/>
+        </IconButton>
+      </ListItemSecondaryAction>
   </ListItem>
 
   );
