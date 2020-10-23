ddabd89b0e2cd696dabf894f3fe8eb597739f4c1
asabogal <asabogal.git@gmail.com>
2020-10-22 21:00:51 -0400

    pass useToggle and render EditTodoForm and Todo content conditionally

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import useToggleState from '../hooks/useToggleState';
+// components
+import EditTodoForm from './EditTodoForm';
 // materialUI
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -10,18 +13,25 @@ import EditIcon from '@material-ui/icons/Edit';
 
 
 const Todo = ({id, task, completed, deleteTodo, updateCompleted}) => {
+
+  const [editing, toggleEditing] = useToggleState(false);
+ 
   return (
     <ListItem>
+    {editing ? <EditTodoForm/>: 
+    <>
       <Checkbox checked={completed} onClick={() => updateCompleted(id)}/>
       <ListItemText style={{textDecoration: completed ? 'line-through' : 'none'}}>{task}</ListItemText>
       <ListItemSecondaryAction>
         <IconButton aria-label='Delete' onClick={() => deleteTodo(id)}>
           <DeleteIcon/>
         </IconButton>
-        <IconButton aria-label='Edit'>
+        <IconButton aria-label='Edit' onClick={toggleEditing}>
           <EditIcon/>
         </IconButton>
       </ListItemSecondaryAction>
+    </>
+    }
   </ListItem>
 
   );
