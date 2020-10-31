771baed3d603da675c76ab78b8705bbe098372e8
asabogal <asabogal.git@gmail.com>
2020-10-31 17:26:41 -0400

    add handleSubmit functionality with id, editTodo, reset and toggleEditing

--- src/components/EditTodoForm.js
+++ src/components/EditTodoForm.js
@@ -4,15 +4,26 @@ import useFormInput from '../hooks/useFormInput';
 // materialUI
 import TextField from '@material-ui/core/TextField';
 
-const EditTodoForm = () => {
+const EditTodoForm = ({id, editTodo, toggleEditing}) => {
 
   const [value, handleChange, reset] = useFormInput('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    editTodo(id, value);
+    reset();
+    toggleEditing(false);
+  }
+
   return (
-    <TextField 
-      margin='normal' 
-      value={value} 
-      onChange={handleChange} 
-      fullWidth/>
+    <form onSubmit={handleSubmit}>
+      <TextField 
+        margin='normal' 
+        value={value} 
+        onChange={handleChange} 
+        fullWidth
+      />
+    </form>
   );
 };
 
